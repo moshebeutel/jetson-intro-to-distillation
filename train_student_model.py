451f80129330690a4176c1bcad711113d60d83c0
moshebeutel <moshebeutel@gmail.com>
2023-11-30 19:23:54 +0200

    train student working

--- train_student_model.py
+++ train_student_model.py
@@ -1,15 +1,12 @@
 from jetson_distillation.utils.stl10_utils import (
-    precompute_clip_stl10_train_image_embeddings,
-    precompute_clip_stl10_test_image_embeddings,
-    precompute_clip_stl10_text_embeddings,
     train_resnet18_from_scratch,
     train_resnet18_zero_shot_train_only,
     train_resnet18_linear_probe_train_only
 )
 
-precompute_clip_stl10_train_image_embeddings()
-precompute_clip_stl10_test_image_embeddings()
-precompute_clip_stl10_text_embeddings()
+print('train_resnet18_from_scratch')
 train_resnet18_from_scratch()
+print('train_resnet18_zero_shot_train_only')
 train_resnet18_zero_shot_train_only()
+print('train_resnet18_linear_probe_train_only')
 train_resnet18_linear_probe_train_only()
